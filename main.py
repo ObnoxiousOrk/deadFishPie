1efd19a01ede5c78ad18236513808600d2576f0a
David Martin <57326392+ObnoxiousOrk@users.noreply.github.com>
2022-01-05 08:35:50 -0500

    initial commits
    working interpreter for base version of deadfish, and a test program taken from https://esolangs.org/wiki/deadfish to ensure it works as expected

---
+++ main.py
@@ -0,0 +1,25 @@
+num = 0
+
+with open("code.df", "r") as file:
+    f = file.read()
+
+code = []
+
+for char in f:
+    if char not in ["i", "d", "s", "o"]:
+        continue
+    code.append(char)
+
+for char in code:
+    if num == -1 or num == 256:
+        num = 0
+
+    match char:
+        case "i":
+            num += 1
+        case "d":
+            num -= 1
+        case "s":
+            num *= num
+        case "o":
+            print(num)
